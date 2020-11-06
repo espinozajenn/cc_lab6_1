afe0e4473e88d35e919b2709c8594494dd1bc4e3
Jennifer Espinoza <jenny@Jennifer-Espinozas-MacBook-2.local>
2020-11-05 18:25:42 -0800

    lab6complete

--- lab6_1.py
+++ lab6_1.py
@@ -1,11 +1,14 @@
-# TODO: Write a function called divisible_by_7. 
+# Write a function called divisible_by_7. 
 # This function must take an integer as a parameter.
 # This function must return True if the passed integer
 # is divisible by 7 or false if not.
 # Note: you should not prompt the user for input. 
+def divisible_by_7(x):
+    if  % 7 ==0:
+        return True
+    return False 
 
-
-# TODO: Write a function called compare_it that takes two parameters. 
+# Write a function called compare_it that takes two parameters. 
 # You should first test if both parameters are integers.
 # If not, return False
 # Then you should test if the parameters are equal.
@@ -13,9 +16,20 @@
 # Finally, test if the parameters are greater than zero.
 # If they are not, return False.
 # If all of these tests pass, return True.
+def compare_it(x,y): 
+    # test they're integers (negative test)
+    if not isinstance(x, int) and not isinstance (y,int):
+    return False
+    # test if equal (negative test)
+    if not x == y
+    return False
+    # test if they're greater than zero (negative test)
+    if x < = 0 and y < =0:
+    return False
+    # all tests pass 
+    return True 
 
-
-# TODO: Write a function called keyword_counter that takes three parameters
+# Write a function called keyword_counter that takes three parameters
 # Parameter one should be a list of words.
 # The second should be set to None by default, and could hold a string to search.
 # The third should be set to None by default, and could hold a filename to search.
@@ -36,8 +50,28 @@
 # one of the words is "fish" your function should count 
 # "fish", "Fish", "FISH" or any other combination of 
 # capitalization as an occurrence. 
-file_name = "test6_1.txt" # DELETE THIS LINE before you build the function.
-with open(file_name, "r") as f:
-   text = f.read().replace("\n", " ")
+def keyword_counter(words, search_string = None, filename = None):
+    if (search_string == None and filename == None) or (search_string !=N
+        return None
+   # search a string for words and count them.
+   for i in range (len(words)):
+        counter = text.count (words [i])
+
+    if seach_string !=None: 
+        text = search_string
+
+        else:
+            with open(file_name, "r") as f:
+                 text = f.read().replace("\n", " ")
+
+    output = dict()
+
+    for word in words: 
+        word = word.lower()
+        output[word] = text.count(word)
+        
+    return = output 
+
+
 
 
